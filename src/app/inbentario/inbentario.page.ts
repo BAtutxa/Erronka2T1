9dc7dca555a3297420ac4201ad723179185ebf28
ibaidp23 <idelpozoe23@fpsanturtzilh.eus>
2025-01-03 11:45:26 +0100

    citas y inbentario avance

--- src/app/inbentario/inbentario.page.ts
+++ src/app/inbentario/inbentario.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-inbentario',
   templateUrl: './inbentario.page.html',
   styleUrls: ['./inbentario.page.scss'],
 })
-export class InbentarioPage implements OnInit {
+export class InbentarioPage {
 
-  constructor() { }
+  constructor(private menuCtrl: MenuController) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.menuCtrl.enable(true); // Habilita el menú al entrar
   }
+
+  ionViewWillLeave() {
+    this.menuCtrl.enable(false); // Deshabilita el menú al salir
+  }
+
 
 }
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent  implements OnInit {
+export class MenuComponent  {
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  constructor(private menuCtrl: MenuController) {
+    // Asegúrate de habilitar el menú
+    this.menuCtrl.enable(true);
+  }
 }
