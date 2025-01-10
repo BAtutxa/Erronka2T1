22126ad7a9a96b81274955b001c1a7093649cf8b
ibaidp <idelpozoe23@fpsanturtzilh.eus>
2025-01-10 09:29:53 +0100

    menu3.0

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,12 +1,20 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent  {
+export class MenuComponent {
+  // Array dinámico para el menú
+  menuItems = [
+    { label: 'Hitzorduak', route: '/citas', icon: 'assets/Fotos/citas.png' },
+    { label: 'Inbentario', route: '/inbentario', icon: 'assets/Fotos/inbentario.png' },
+    { label: 'Taldeak', route: '/grupos', icon: 'assets/Fotos/equipo.png' },
+    { label: 'Asistentzia', route: '/asistentzia', icon: 'assets/Fotos/asist.png' },
+    { label: 'Ticket', route: '/ticket', icon: 'assets/Fotos/recibo.png' },
+    { label: 'Garbiketa', route: '/garbiketa', icon: 'assets/Fotos/garbi.png' },
+  ];
 
-  constructor() {
-
-  }
+  constructor() {}
 }
