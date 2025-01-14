f5fb1c8879ffaa7c35c823f648cb7f459fcf1839
ibaidp <idelpozoe23@fpsanturtzilh.eus>
2025-01-14 10:12:11 +0100

    ticket 2.0

--- src/app/ticket/ticket.page.ts
+++ src/app/ticket/ticket.page.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { jsPDF } from 'jspdf';
-
 
 @Component({
   selector: 'app-ticket',
@@ -10,6 +9,17 @@ import { jsPDF } from 'jspdf';
   styleUrls: ['./ticket.page.scss'],
 })
 export class TicketPage {
+
+  ticket = {
+    cliente: '',
+    fecha: '',
+    hora: '',
+    servicio: '',
+    precio: 0,
+    pagado: 0,
+    vueltas: 0,
+    metodoPago: ''
+  };
 
   constructor(private router: Router, private alertController: AlertController) { }
 
@@ -32,16 +42,37 @@ export class TicketPage {
       ],
     });
 
-    await alert.present(); // Es necesario presentar el alert
+    await alert.present();
   }
 
   logout(): void {
-    this.router.navigate(['/home']); // Navegar a la página de inicio (puedes cambiar la ruta según tu aplicación)
+    this.router.navigate(['/home']);
   }
 
- sortuPDF(){
-  const pdf = new jsPDF();
+  calcularVuelta(): void {
+    if (this.ticket.pagado && this.ticket.precio) {
+      this.ticket.vueltas = this.ticket.pagado - this.ticket.precio;
+    }
+  }
 
+  sortuPDF(ticketForm: any) {
+    if (ticketForm.valid) {
+      // Generar PDF solo si el formulario es válido
+      const pdf = new jsPDF();
+      pdf.text('Ticket de Cita de Peluquería', 20, 20);
+      pdf.text(`Cliente: ${this.ticket.cliente}`, 20, 30);
+      pdf.text(`Fecha: ${this.ticket.fecha}`, 20, 40);
+      pdf.text(`Hora: ${this.ticket.hora}`, 20, 50);
+      pdf.text(`Servicio: ${this.ticket.servicio}`, 20, 60);
+      pdf.text(`Precio: ${this.ticket.precio}€`, 20, 70);
+      pdf.text(`Monto Pagado: ${this.ticket.pagado}€`, 20, 80);
+      pdf.text(`Vueltas: ${this.ticket.vueltas}€`, 20, 90);
+      pdf.text(`Método de Pago: ${this.ticket.metodoPago}`, 20, 100);
+      pdf.save('ticket_peluqueria.pdf');
+    } else {
+      // Mostrar un mensaje si el formulario no es válido
+      alert('Por favor, rellena todos los campos obligatorios.');
+    }
+  }
   
-  }
 }
